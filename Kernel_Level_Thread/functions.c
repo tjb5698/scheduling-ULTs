0a65435c3b4b23fba4f1f5b990f7101c8246b715
Mitchel Myers <doublemx2@gmail.com>
2019-02-11 22:34:20 -0500

    Formatting

--- Kernel_Level_Thread/functions.c
+++ Kernel_Level_Thread/functions.c
@@ -2,64 +2,67 @@
 
 void *counter(void *arr)
 {
-  long tid;
-  pid_t id;
-  int temp;
-  id = syscall(SYS_gettid);
-  int *ar = arr;
+    long tid;
+    pid_t id;
+    int temp;
+    id = syscall(SYS_gettid);
+    int *ar = arr;
 
-  struct timespec begin;
-  struct timespec current;
-  clock_gettime(CLOCK_MONOTONIC, &begin);
+    struct timespec begin;
+    struct timespec current;
+    clock_gettime(CLOCK_MONOTONIC, &begin);
 
-  long long int rf = 0;
-  while (1)
-  {
-    rf++;
-
-    clock_gettime(CLOCK_MONOTONIC, &current);
-    if (((long long)current.tv_sec - (long long)begin.tv_sec) >= 10.0)
+    long long int rf = 0;
+    while (1)
     {
+        rf++;
 
-      break;
+        clock_gettime(CLOCK_MONOTONIC, &current);
+        if (((long long)current.tv_sec - (long long)begin.tv_sec) >= 10.0)
+        {
+
+            break;
+        }
     }
-  }
-  temp = (int)id % 100;
-  printf("temp = %d\n", temp);
-  ar[temp] = rf;
-  pthread_exit(NULL);
+    temp = (int)id % 100;
+    printf("temp = %d\n", temp);
+    ar[temp] = rf;
+    pthread_exit(NULL);
 }
 
 void *sleeping(void *arr)
 {
-  long tid;
-  pid_t id;
-  id = syscall(SYS_gettid);
-  int *ar = arr;
+    long tid;
+    pid_t id;
+    id = syscall(SYS_gettid);
+    int *ar = arr;
 
-  int temp;
+    int temp;
 
-  struct timespec begin;
-  struct timespec current;
-  clock_gettime(CLOCK_MONOTONIC, &begin);
+    struct timespec begin;
+    struct timespec current;
+    clock_gettime(CLOCK_MONOTONIC, &begin);
 
-  long long int rf = 0;
-  while (1)
-  {
-    rf++;
-    clock_gettime(CLOCK_MONOTONIC, &current);
+    long long int rf = 0;
     if (id % 2 == 0)
     {
-      if (((long long)current.tv_sec - (long long)begin.tv_sec) == 5.0)
         sleep(3);
     }
-
-    if (((long long)current.tv_sec - (long long)begin.tv_sec) >= 10.0)
+    else
     {
-      break;
+        sleep(2);
     }
-  }
-  temp = id % 100;
-  ar[temp] = rf;
-  pthread_exit(NULL);
+    while (1)
+    {
+        rf++;
+        clock_gettime(CLOCK_MONOTONIC, &current);
+
+        if (((long long)current.tv_sec - (long long)begin.tv_sec) >= 10)
+        {
+            break;
+        }
+    }
+    temp = id % 100;
+    ar[temp] = rf;
+    pthread_exit(NULL);
 }
